ba84cc4ab74ae07cfea79bd39ef3db599e0fbdd3
ArtByMilky <74256694+artbymilky@users.noreply.github.com>
2021-02-15 12:04:21 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+ 
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.get('/',(req, res) => {
+
+    res.render('home');
+
+    app.get('/contact', (req, res) => {
+        res.render('contact', { contactContenido: contactContent });
+      });
+
+});
+
+
+
+
+
+
+app.listen(4000,() =>{
+    console.log('Server started on local port 4000 bitchaos');
+});
+
+
+
+
